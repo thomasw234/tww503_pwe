5036d7dff852632c230de98dd7a7f3161d90955e
Thomas Wormald <thomaswormald@nas10-240-36-125.york.ac.uk>
2014-04-20 16:41:24 +0100

    More work on report

---
+++ code/contrivedWhile.java
@@ -0,0 +1,13 @@
+private void loopSample() {	
+	int i = 0;	
+	while (i < 5) {	
+		int j = 0;	
+		if (i == 4)
+			break;			
+		while (j < 5) {		
+			j++;
+			if (j == 3)
+				break;				
+		}		
+	}
+}
